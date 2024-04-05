04b3c2f4542209f20a7a1aaecf844b5857643a01
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-04-04 21:57:18 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-red-100 text-center text-5xl'>Jelou yesi</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'custom': '2px 2px 4px rgba(0, 0, 0, 0.2)'
+      },
+      colors: { // variables for tailwind classes -> example: bg-parimary || text-error
+        'primary': '#3652AD',
+        'secondary': '#6CFBCE',
+        'secondary-light': '#E6F4E2',
+        'third': '#20C198',
+        'secondary-dark': '#008A65',
+        'dark-green': 'rgba(3, 61, 47)',
+        'error': 'hsl(0, 100%, 63%)',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'lora': ['Lora', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
