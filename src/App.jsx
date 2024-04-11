40763bd81c49440565b2d12ff3194c1c15c4583a
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-04-11 11:30:14 -0500

    feat: initial folders structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import './App.css'
+import { About } from './components/About/About'
+import { BrandCarousel } from './components/BrandCarousel/BrandCarousel'
+import { Hero } from './components/Hero/Hero'
+import { Navbar } from './components/Navbar/Navbar'
+import { Service } from './components/Service/Service'
+import { Testimonials } from './components/Testimonials/Testimonials'
+import { Contact } from "./components/Contact/Contact";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-red-100 text-center text-5xl'>Jelou yesi</h1>
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Service />
+        <BrandCarousel />
+        <Testimonials />
+        <Contact/>
+      </main>
+      <Footer/>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,15 @@ export default {
       boxShadow: {
         'custom': '2px 2px 4px rgba(0, 0, 0, 0.2)'
       },
-      colors: { // variables for tailwind classes -> example: bg-parimary || text-error
+      colors: {
         'primary': '#3652AD',
         'secondary': '#6CFBCE',
         'secondary-light': '#E6F4E2',
         'third': '#20C198',
-        'secondary-dark': '#008A65',
+        'secondary-dark': '#008A66',
         'dark-green': 'rgba(3, 61, 47)',
+        'footer': '#02513D',
+        'gray': '#F0F1F8',
         'error': 'hsl(0, 100%, 63%)',
       },
       fontFamily: {
